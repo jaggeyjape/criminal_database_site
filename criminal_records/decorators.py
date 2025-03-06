03dc9b0d6ef3fc6d7b77f0387a6875c7d0739fc5
Amna1710 <98222206+Amna1710@users.noreply.github.com>
2025-03-06 22:36:18 +0500

    Update decorators.py

--- criminal_records/decorators.py
+++ criminal_records/decorators.py
@@ -5,6 +5,6 @@ def login_required(view_func):
         if not request.session.get("is_logged_in",False):  # Ensure session flag exists
             return redirect("login")
         if "authorized" not in request.GET:
-            return (request.path + "?authorized=true")# Redirect to login page
+            return redirect(request.path + "?authorized=true")# Redirect to login page
         return view_func(request, *args, **kwargs)
     return wrapper
